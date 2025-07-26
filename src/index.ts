3486be52154f36280d6d0d45554eedad6024f518
Ramzy Saad <www.ramzysaad16@gmail.com>
2025-07-26 16:23:20 +0300

    start in users

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import  http from 'http';
 import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
+
 
 const app = express();
 
@@ -15,3 +17,11 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
+server.listen(8080,()=>{
+    console.log('Server working on 8080.');
+});
+
+const mongo_url = "mongodb://127.0.0.1:27017/appTypeScript";
+mongoose.Promise = Promise;
+mongoose.connect(mongo_url);
+mongoose.connection.on('error',(error:Error)=>{ console.log(error);});
