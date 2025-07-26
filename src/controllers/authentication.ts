1c29f2dae1e612cde072b72c65b5aa1bf755844b
Ramzy Saad <www.ramzysaad16@gmail.com>
2025-07-26 17:32:16 +0300

    start in routes

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import { createUser, getUserByEmail } from "../db/users";
+import { authentication, random } from "../helpers";
+
+export const register = async (req: express.Request, res:express.Response)=>{
+    try{
+        const {email, password, name} = req.body;
+        if(!email || !password || !name){
+            return res.sendStatus(400);
+        }
+        const existingUser = await getUserByEmail(email);
+        if (existingUser) {
+            return res.sendStatus(400);
+        }
+
+        const salt = random();
+        const user = await createUser({
+            email,name,authentication:{
+                password:authentication(salt,password),
+                salt,
+            }
+        });
+        return res.status(200).json(user).end();
+    }catch(error){
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
